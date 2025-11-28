095011af73cb064df4cf194b074a958f15163f32
Daria-18 <dariapetrov1807@gmail.com>
2025-11-28 10:17:43 +0100

    Clase 28-11

---
+++ JavaScript/js/ejemplo.js
@@ -0,0 +1,11 @@
+/* EJERCICIO 1
+Función que recibe una base y un exponente y devuelve base ** exponente
+Mostrar en consola el resultado para 2³ y 5²
+*/
+
+function potencia(base,exponente){
+    return base ** exponente
+}
+
+console.log("2 elevado a 3 es", potencia(2,3))
+console.log("5 elevado a 2 es", potencia(5,2))
