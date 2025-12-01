35e2592086beb9ac23218b5b51a59d6e74d3809e
Daria-18 <dariapetrov1807@gmail.com>
2025-12-01 10:20:21 +0100

    clase 1-12

--- JavaScript/js/ejemplo.js
+++ JavaScript/js/ejemplo.js
@@ -9,3 +9,18 @@ function potencia(base,exponente){
 
 console.log("2 elevado a 3 es", potencia(2,3))
 console.log("5 elevado a 2 es", potencia(5,2))
+
+let alumno = {
+    nombre: "Carlos",
+    "Nota Final": 8.5
+}
+
+console.log("Notación punto alumno.nombre:",alumno.nombre)
+
+//No se accede con un punto a la propiedad -> Syntax Error
+console.log("Notación corchete alumno[nota final]:",alumno["Nota Final"])
+
+//Acceso dinamico
+let clave = "nombre"
+console.log("Acceso usando la variable clave:", alumno[clave])
+
